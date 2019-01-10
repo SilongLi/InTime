052b6a5e261467654663ef2ad895e3d0ba6703ef
lisilong <lisilong@tuandai.com>
2019-01-10 12:09:24 +0800

    init InTime

---
+++ InTime/InTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InTime
+//
+//  Created by lisilong on 2019/1/10.
+//  Copyright © 2019 BruceLi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
