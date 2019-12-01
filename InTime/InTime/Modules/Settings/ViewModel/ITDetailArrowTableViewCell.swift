64eaf1d9cb031ec1fb2f943f298005db0234e41b
Bruce Li <lisilongios@163.com>
2019-12-01 12:03:10 +0800

    设置中心

---
+++ InTime/InTime/Modules/Settings/ViewModel/ITDetailArrowTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ITDetailArrowTableViewCell.swift
+//  InTime
+//
+//  Created by lisilong on 2019/12/1.
+//  Copyright © 2019 BruceLi. All rights reserved.
+//
+
+import UIKit
+
+class ITDetailArrowTableViewCell: BaseTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
